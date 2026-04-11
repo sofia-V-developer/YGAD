9e6c6f118be8119715f9b1b16ebb54b0b014c4c7
Sofia <masha.2153a@mail.ru>
2026-04-11 20:30:56 +0300

    - базовая аватарка при входе

--- app/src/main/java/com/example/ygad/SubjectDao.java
+++ app/src/main/java/com/example/ygad/SubjectDao.java
@@ -8,13 +8,11 @@ import java.util.List;
 @Dao
 public interface SubjectDao {
     @Query("SELECT * FROM subjects ORDER BY name")
-    List<com.example.ygad.Subject> getAll();
+    List<Subject> getAll();
 
-    @Query("SELECT * FROM subjects WHERE name LIKE :query")
-    List<com.example.ygad.Subject> search(String query);
-
-    @Insert
-    long insert(com.example.ygad.Subject subject);
     @Query("SELECT * FROM subjects WHERE name LIKE :query OR teacher LIKE :query")
     List<Subject> search(String query);
+
+    @Insert
+    long insert(Subject subject);
 }
